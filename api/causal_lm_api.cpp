EunjuYang/nntrainer

[CausalLM] add ChatTemplate
- This commit adds a feature of chat template.
- This commit implements chat template (supporting qwen3)

Signed-off-by: Eunju Yang <ej.yang@samsung.com>

Applications/CausalLM/api/causal_lm_api.cpp
@@ -21,6 +21,7 @@
 #include <vector>
 
 #include "causal_lm.h"
+#include "chat_template.h"
 #include "gemma3_causallm.h"
 #include "gptoss_cached_slim_causallm.h"
 #include "gptoss_causallm.h"
@@ -46,6 +47,7 @@ static bool g_use_chat_template = false;
 static bool g_verbose = false;
 static std::string g_last_output = "";
 static double g_initialization_duration_ms = 0.0;
+static causallm::ChatTemplate g_chat_template;
 
 static std::map<std::string, std::string> g_model_path_map = {
   {"QWEN3-0.6B", "qwen3-0.6b"},
@@ -134,6 +136,12 @@ static const char *get_model_name_from_type(ModelType type) {
 
 static std::string apply_chat_template(const std::string &architecture,
                                        const std::string &input) {
+  // Use dynamic chat template from tokenizer_config.json if available
+  if (g_chat_template.isAvailable()) {
+    return g_chat_template.apply(input);
+  }
+
+  // Fallback: hardcoded per-architecture templates
   if (architecture == "LlamaForCausalLM") {
     // Llama 2/3 chat format: [INST] {prompt} [/INST]
     return "[INST] " + input + " [/INST]";
@@ -144,8 +152,6 @@ static std::string apply_chat_template(const std::string &architecture,
              architecture == "Qwen3CachedSlimMoeForCausalLM") {
     // Qwen chat format
     // <|im_start|>user\n{prompt}<|im_end|>\n<|im_start|>assistant\n
-    // Note: assuming model handles tokenizer specific special tokens or we
-    // might need to handle them raw if tokenizer enabled
     return "<|im_start|>user\n" + input + "<|im_end|>\n<|im_start|>assistant\n";
   } else if (architecture == "Gemma3ForCausalLM") {
     // Gemma chat format:
@@ -466,6 +472,12 @@ ErrorCode loadModel(BackendType compute, ModelType modeltype,
         std::string t_file = nntr_cfg["tokenizer_file"];
         nntr_cfg["tokenizer_file"] = model_dir_path + "/" + t_file;
       }
+    }
+
+    // Load chat template from tokenizer_config.json if available
+    std::string tc_path = model_dir_path + "/tokenizer_config.json";
+    if (check_file_exists(tc_path)) {
+      g_chat_template = causallm::ChatTemplate::fromFile(tc_path);
     }
 
     // Construct weight file path

Applications/CausalLM/chat_template.h
@@ -1,0 +1,106 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2026 Samsung Electronics Co., Ltd. All Rights Reserved.
+ *
+ * @file    chat_template.h
+ * @date    10 Apr 2026
+ * @brief   Chat template support using tokenizer_config.json
+ * @see     https://github.com/nntrainer/nntrainer
+ * @author  Eunju Yang <ej.yang@samsung.com>
+ * @bug     No known bugs except for NYI items
+ */
+
+#ifndef __CHAT_TEMPLATE_H__
+#define __CHAT_TEMPLATE_H__
+
+#include <string>
+#include <vector>
+
+#include "json.hpp"
+
+namespace causallm {
+
+using json = nlohmann::json;
+
+/**
+ * @brief Chat message structure for multi-turn conversations
+ */
+struct ChatMessage {
+  std::string role;    // "system", "user", "assistant"
+  std::string content; // message content
+};
+
+/**
+ * @brief Chat template class that reads and applies HuggingFace chat templates
+ *
+ * Loads chat_template from tokenizer_config.json and renders it using a
+ * minimal Jinja2 subset renderer. Supports common constructs used in
+ * HuggingFace chat templates: for loops, if/elif/else, variable access,
+ * string operations, loop variables, and filters.
+ */
+class ChatTemplate {
+public:
+  /**
+   * @brief Default constructor (no template loaded)
+   */
+  ChatTemplate();
+
+  /**
+   * @brief Load chat template from tokenizer_config.json
+   * @param tokenizer_config_path Path to tokenizer_config.json
+   * @return ChatTemplate instance
+   */
+  static ChatTemplate fromFile(const std::string &tokenizer_config_path);
+
+  /**
+   * @brief Apply template to multi-turn messages
+   * @param messages Vector of ChatMessage (role + content)
+   * @param add_generation_prompt Whether to add generation prompt at end
+   * @return Formatted prompt string
+   */
+  std::string apply(const std::vector<ChatMessage> &messages,
+                    bool add_generation_prompt = true) const;
+
+  /**
+   * @brief Apply template to a single user input (convenience)
+   * @param user_input Raw user input string
+   * @param add_generation_prompt Whether to add generation prompt at end
+   * @return Formatted prompt string
+   */
+  std::string apply(const std::string &user_input,
+                    bool add_generation_prompt = true) const;
+
+  /**
+   * @brief Check if a chat template is loaded and available
+   * @return true if template is available
+   */
+  bool isAvailable() const;
+
+  /**
+   * @brief Get BOS token
+   */
+  std::string getBosToken() const;
+
+  /**
+   * @brief Get EOS token
+   */
+  std::string getEosToken() const;
+
+private:
+  std::string template_str_;
+  std::string bos_token_;
+  std::string eos_token_;
+  bool available_ = false;
+
+  /**
+   * @brief Render a Jinja2 template with the given context
+   * @param tmpl Jinja2 template string
+   * @param context JSON object with template variables
+   * @return Rendered string
+   */
+  std::string render(const std::string &tmpl, const json &context) const;
+};
+
+} // namespace causallm
+
+#endif // __CHAT_TEMPLATE_H__

Applications/CausalLM/main.cpp
@@ -30,6 +30,7 @@
 #include <factory.h>
 
 #include "causal_lm.h"
+#include "chat_template.h"
 #include "embedding_gemma.h"
 #include "gemma3_causallm.h"
 #include "gptoss_cached_slim_causallm.h"
@@ -46,6 +47,7 @@
 
 #include <atomic>
 #include <chrono>
+#include <filesystem>
 #include <thread>
 
 using json = nlohmann::json;
@@ -243,9 +245,24 @@ int main(int argc, char *argv[]) {
       architecture = resolve_architecture(model_type, architecture);
     }
 
+    // Load chat template from tokenizer_config.json (if available)
+    causallm::ChatTemplate chat_tmpl;
+    std::string tokenizer_config_path = model_path + "/tokenizer_config.json";
+    if (std::filesystem::exists(tokenizer_config_path)) {
+      chat_tmpl = causallm::ChatTemplate::fromFile(tokenizer_config_path);
+      if (chat_tmpl.isAvailable()) {
+        std::cout << "[Info] Chat template loaded from tokenizer_config.json"
+                  << std::endl;
+      }
+    }
+
     // Determine input text
     if (argc >= 3) {
       input_text = argv[2];
+      // Apply chat template to raw user input if available
+      if (chat_tmpl.isAvailable()) {
+        input_text = chat_tmpl.apply(input_text);
+      }
     } else {
       if (nntr_cfg.contains("chat_input")) {
         if (architecture == "Gemma3ForCausalLM") {

EunjuYang/Quick.AI

api/causal_lm_api.cpp
@@ -21,6 +21,7 @@
 #include <vector>
 
 #include "causal_lm.h"
+#include "chat_template.h"
 #include "gemma3_causallm.h"
 #include "gptoss_cached_slim_causallm.h"
 #include "gptoss_causallm.h"
@@ -46,6 +47,7 @@ static bool g_use_chat_template = false;
 static bool g_verbose = false;
 static std::string g_last_output = "";
 static double g_initialization_duration_ms = 0.0;
+static quick_dot_ai::ChatTemplate g_chat_template;
 
 static std::map<std::string, std::string> g_model_path_map = {
   {"QWEN3-0.6B", "qwen3-0.6b"},
@@ -134,6 +136,12 @@ static const char *get_model_name_from_type(ModelType type) {
 
 static std::string apply_chat_template(const std::string &architecture,
                                        const std::string &input) {
+  // Use dynamic chat template from tokenizer_config.json if available
+  if (g_chat_template.isAvailable()) {
+    return g_chat_template.apply(input);
+  }
+
+  // Fallback: hardcoded per-architecture templates
   if (architecture == "LlamaForCausalLM") {
     // Llama 2/3 chat format: [INST] {prompt} [/INST]
     return "[INST] " + input + " [/INST]";
@@ -144,8 +152,6 @@ static std::string apply_chat_template(const std::string &architecture,
              architecture == "Qwen3CachedSlimMoeForCausalLM") {
     // Qwen chat format
     // <|im_start|>user\n{prompt}<|im_end|>\n<|im_start|>assistant\n
-    // Note: assuming model handles tokenizer specific special tokens or we
-    // might need to handle them raw if tokenizer enabled
     return "<|im_start|>user\n" + input + "<|im_end|>\n<|im_start|>assistant\n";
   } else if (architecture == "Gemma3ForCausalLM") {
     // Gemma chat format:
@@ -466,6 +472,12 @@ ErrorCode loadModel(BackendType compute, ModelType modeltype,
         std::string t_file = nntr_cfg["tokenizer_file"];
         nntr_cfg["tokenizer_file"] = model_dir_path + "/" + t_file;
       }
+    }
+
+    // Load chat template from tokenizer_config.json if available
+    std::string tc_path = model_dir_path + "/tokenizer_config.json";
+    if (check_file_exists(tc_path)) {
+      g_chat_template = quick_dot_ai::ChatTemplate::fromFile(tc_path);
     }
 
     // Construct weight file path

chat_template.h
@@ -1,0 +1,106 @@
+// SPDX-License-Identifier: Apache-2.0
+/**
+ * Copyright (C) 2026 Samsung Electronics Co., Ltd. All Rights Reserved.
+ *
+ * @file    chat_template.h
+ * @date    10 Apr 2026
+ * @brief   Chat template support using tokenizer_config.json
+ * @see     https://github.com/nntrainer/nntrainer
+ * @author  Eunju Yang <ej.yang@samsung.com>
+ * @bug     No known bugs except for NYI items
+ */
+
+#ifndef __CHAT_TEMPLATE_H__
+#define __CHAT_TEMPLATE_H__
+
+#include <string>
+#include <vector>
+
+#include "json.hpp"
+
+namespace quick_dot_ai {
+
+using json = nlohmann::json;
+
+/**
+ * @brief Chat message structure for multi-turn conversations
+ */
+struct ChatMessage {
+  std::string role;    // "system", "user", "assistant"
+  std::string content; // message content
+};
+
+/**
+ * @brief Chat template class that reads and applies HuggingFace chat templates
+ *
+ * Loads chat_template from tokenizer_config.json and renders it using a
+ * minimal Jinja2 subset renderer. Supports common constructs used in
+ * HuggingFace chat templates: for loops, if/elif/else, variable access,
+ * string operations, loop variables, and filters.
+ */
+class ChatTemplate {
+public:
+  /**
+   * @brief Default constructor (no template loaded)
+   */
+  ChatTemplate();
+
+  /**
+   * @brief Load chat template from tokenizer_config.json
+   * @param tokenizer_config_path Path to tokenizer_config.json
+   * @return ChatTemplate instance
+   */
+  static ChatTemplate fromFile(const std::string &tokenizer_config_path);
+
+  /**
+   * @brief Apply template to multi-turn messages
+   * @param messages Vector of ChatMessage (role + content)
+   * @param add_generation_prompt Whether to add generation prompt at end
+   * @return Formatted prompt string
+   */
+  std::string apply(const std::vector<ChatMessage> &messages,
+                    bool add_generation_prompt = true) const;
+
+  /**
+   * @brief Apply template to a single user input (convenience)
+   * @param user_input Raw user input string
+   * @param add_generation_prompt Whether to add generation prompt at end
+   * @return Formatted prompt string
+   */
+  std::string apply(const std::string &user_input,
+                    bool add_generation_prompt = true) const;
+
+  /**
+   * @brief Check if a chat template is loaded and available
+   * @return true if template is available
+   */
+  bool isAvailable() const;
+
+  /**
+   * @brief Get BOS token
+   */
+  std::string getBosToken() const;
+
+  /**
+   * @brief Get EOS token
+   */
+  std::string getEosToken() const;
+
+private:
+  std::string template_str_;
+  std::string bos_token_;
+  std::string eos_token_;
+  bool available_ = false;
+
+  /**
+   * @brief Render a Jinja2 template with the given context
+   * @param tmpl Jinja2 template string
+   * @param context JSON object with template variables
+   * @return Rendered string
+   */
+  std::string render(const std::string &tmpl, const json &context) const;
+};
+
+} // namespace quick_dot_ai
+
+#endif // __CHAT_TEMPLATE_H__

main.cpp
@@ -30,6 +30,7 @@
 #include <factory.h>
 
 #include "causal_lm.h"
+#include "chat_template.h"
 #include "embedding_gemma.h"
 #include "gemma3_causallm.h"
 #include "gptoss_cached_slim_causallm.h"
@@ -46,6 +47,7 @@
 
 #include <atomic>
 #include <chrono>
+#include <filesystem>
 #include <thread>
 
 using json = nlohmann::json;
@@ -243,9 +245,24 @@ int main(int argc, char *argv[]) {
       architecture = resolve_architecture(model_type, architecture);
     }
 
+    // Load chat template from tokenizer_config.json (if available)
+    quick_dot_ai::ChatTemplate chat_tmpl;
+    std::string tokenizer_config_path = model_path + "/tokenizer_config.json";
+    if (std::filesystem::exists(tokenizer_config_path)) {
+      chat_tmpl = quick_dot_ai::ChatTemplate::fromFile(tokenizer_config_path);
+      if (chat_tmpl.isAvailable()) {
+        std::cout << "[Info] Chat template loaded from tokenizer_config.json"
+                  << std::endl;
+      }
+    }
+
     // Determine input text
     if (argc >= 3) {
       input_text = argv[2];
+      // Apply chat template to raw user input if available
+      if (chat_tmpl.isAvailable()) {
+        input_text = chat_tmpl.apply(input_text);
+      }
     } else {
       if (nntr_cfg.contains("chat_input")) {
         if (architecture == "Gemma3ForCausalLM") {